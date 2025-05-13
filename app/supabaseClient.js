bf584d3c5de0912066c5897cd90c47ed06c9a634
sanj <sanjna.sur10@gmail.com>
2025-05-13 18:23:54 -0400

    changed config

--- app/supabaseClient.js
+++ app/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://niqomrgbdcegxorlifel.supabase.co'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-const supabasePrivateKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5pcW9tcmdiZGNlZ3hvcmxpZmVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE1NTM4MTcsImV4cCI6MjA1NzEyOTgxN30.6qAVJlUjbd-JnEDMBNPdyavb_-97HZcB7ECvmX7Pfus'
+const supabasePrivateKey = process.env.NEXT_PUBLIC_SUPABASE_PRIVATE_KEY
 
 export const supabase = createClient(supabaseUrl, supabasePrivateKey)
